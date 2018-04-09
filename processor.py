489ff34b0dfc9230e41774bab4beb96063bdb168
davint <davint@davint.com>
2018-04-09 16:25:47 -0400

    Added user report, retweet_count report and hourly traffic reports

--- processor.py
+++ processor.py
@@ -1,10 +1,11 @@
 import ijson
 import unidecode
 import re
+from datetime import datetime
 
 #file path for stopwords and json exports
 stopwords_file_path = 'stopwords.txt'
-json_export_file_path = 'lula.json'
+json_export_file_path = 'the_voice4.json'
 
 #read files
 with open(stopwords_file_path) as f:
@@ -52,16 +53,22 @@ current_string = ''
 placeholder_list = [None] * len(items)
 master_word_list = []
 master_hashtag_list = []
+master_user_list = []
 
 for idx, item in enumerate(items):
     #word_list = re.sub("[^\w]", " ", items[idx]['text']).split()
-    word_list = re.sub(r'[.!,;?]', ' ', items[idx]['text']).split()
+    word_list = re.sub(r'[.!,;:?]', ' ', items[idx]['text']).split()
     word_list = sanitize_3(word_list, stopwords)
     for idx2, word in enumerate(word_list):
         if word_list[idx2][:1] == '#':
             current_string += ' ' + word_list[idx2]
             master_hashtag_list.append(word_list[idx2])
-        else:
+        elif word_list[idx2][:1] == '@':
+            current_string += ' ' + word_list[idx2]
+            master_user_list.append(word_list[idx2])
+        elif word_list[idx2][:3] == '//t':              #dirty workaround
+            pass
+        elif 'co/' not in word_list[idx2]:
             current_string += ' ' + word_list[idx2]
             master_word_list.append(word_list[idx2])
     placeholder_list[idx] = current_string
@@ -75,6 +82,7 @@ del placeholder_list[:]
 #adapted from https://github.com/kevinschaul/Word-Rank/blob/master/wordRank.py
 l = {}
 hashtag_dict = {}
+user_dict = {}
 
 for word in master_word_list:
     # if word is in dictionary, increment the value
@@ -90,12 +98,92 @@ for hashtag in master_hashtag_list:
     else:
         hashtag_dict[hashtag] = 1
 
+for user in master_user_list:
+    if user in user_dict:
+        user_dict[user] += 1
+    else:
+        user_dict[user] = 1
+
 # this prints the dict out sorted by value in descending order
 print '\n#########   Printing ranking of words   #########\n'
 for key, value in sorted(l.iteritems(), reverse=True, key=lambda (k,v): (v,k)):
-    if value > 19:
+    if value > 100:
         print '%s: %s' % (key, value)
 
 print '\n#########   Printing ranking of hashtags   #########\n'
 for key, value in sorted(hashtag_dict.iteritems(), reverse=True, key=lambda (k,v): (v,k)):
-        print '%s: %s' % (key, value)
+        if value > 10:
+            print '%s: %s' % (key, value)
+
+print '\n#########   Printing ranking of most mentioned users   #########\n'
+for key, value in sorted(user_dict.iteritems(), reverse=True, key=lambda (k,v): (v,k)):
+        if value > 100:
+            print '%s: %s' % (key, value)
+
+most_retweeted_list = []
+list_is_full = False
+
+for tweet in items:
+    if 'rt @' in tweet['text']:
+        pass
+    elif tweet['retweet_count'] > 24:
+        most_retweeted_list.append(tweet)
+
+count = 0
+print '\n#########   Printing most retweeted tweets   #########\n'
+for tweet in sorted(most_retweeted_list, key = lambda x: x['retweet_count'], reverse=True):
+    print '@' + tweet['screen_name']
+    print tweet['text']
+    print 'Retweeted: ' , tweet['retweet_count']
+    print '\n'
+    if count == 30:
+        break
+    count+=1
+
+hour_list = []
+count = 0
+for tweet in items:
+    current_date = datetime.strptime(tweet['created_at'],'%Y-%m-%dT%H:%M:%SZ')
+    hour_list.append(current_date.hour)
+
+hour_dict = {}
+
+
+for hour in hour_list:
+    if hour in hour_dict:
+        hour_dict[hour] += 1
+    else:
+        hour_dict[hour] = 1
+
+hour_minute_list = []
+for tweet in items:
+    current_date = datetime.strptime(tweet['created_at'],'%Y-%m-%dT%H:%M:%SZ')
+
+    if int(str(current_date.minute)[:1]) >= 6:
+        string = str(current_date.hour) + ':' + '00'
+    elif int(str(current_date.minute)[:1]) < 6 and current_date.minute < 10:
+        string = str(current_date.hour) + ':' + '00'
+    else:
+        string = str(current_date.hour) + ':' + str(current_date.minute)[:1] + '0'
+    hour_minute_list.append(string)
+
+hour_minute_dict = {}
+
+for hour_minute in hour_minute_list:
+    if hour_minute in hour_minute_dict:
+        hour_minute_dict[hour_minute] += 1
+    else:
+        hour_minute_dict[hour_minute] = 1
+
+
+
+#going to break on cases that go from 23 to midnight or backwards
+print '\n#########   Printing hourly traffic   #########\n'
+for key, value in sorted(hour_dict.iteritems(), reverse=True, key=lambda (k,v): (k,v)):
+    print '%s: %s' % (key-3, value)
+
+print '\n#########   Printing extended hourly traffic   #########\n'
+for key, value in sorted(hour_minute_dict.iteritems(), reverse=True, key=lambda (k,v): (int(k.replace(':', '')),v)):
+    print '%s: %s' % (str(int(key[:2])-3) + ':' + key[3] + key[4], value)
+
+print '\n\n'
